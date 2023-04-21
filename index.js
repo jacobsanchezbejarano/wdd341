99246489c920befea11c6a111fb6ca7c4a0ff1b0
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-04-21 02:10:21 -0400

    Simple web service

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const hostname = this.window || '127.0.0.1';
+const hostname = '127.0.0.1';
 const port = process.env.PORT || 3030;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Nola Justiniano');
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Nola Justiniano');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.use(routes);
+
+app.listen(port, ()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
