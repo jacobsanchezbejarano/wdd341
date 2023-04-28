c6c0e3a73542e5ff16b3da40cb1d11cf43680e03
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-04-28 04:22:12 -0400

    Changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 get_name = (req, res) => {
-    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 200;
     res.send('Nola Justiniano');
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
 const express = require('express');
 const routes = express.Router();
 const functions = require('../controllers/');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger-output.json');
 
 routes.get('/', functions.get_name);
 routes.use('/contacts', require('./contacts'));
-var options = {
-    explorer: true
-  };
-routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 
