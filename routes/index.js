e62c2a4c6862b3cc940c7739fa3ffa7aa7a1a041
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-04-28 02:41:49 -0400

    Swagger

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const routes = express.Router();
 const functions = require('../controllers/');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
 
 routes.get('/', functions.get_name);
 routes.use('/contacts', require('./contacts'));
+var options = {
+    explorer: true
+  };
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
+
 
 module.exports = routes;
