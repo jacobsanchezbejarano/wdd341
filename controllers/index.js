151fe70b0b3c511db53fb95ded690e65e82c2c39
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-04-29 16:48:41 -0400

    fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,9 @@
 get_name = (req, res) => {
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Z-Key');
     res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.statusCode = 200;
     res.send('Nola Justiniano');
 }
