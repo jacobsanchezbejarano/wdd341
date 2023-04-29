6036201cf1d0db4d4e21557f368b9932c0965616
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-04-29 02:54:10 -0400

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 3030;
 const hostname = '127.0.0.1';
 
-app.use(bodyParser.json()).use('/', require('./routes'))
+app.use('/', require('./routes'))
   .use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Z-Key');
@@ -13,6 +13,7 @@ app.use(bodyParser.json()).use('/', require('./routes'))
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
   })
+  .use(bodyParser.json())
   ;
 
 mongodb.initDb((err, mongodb) => {
