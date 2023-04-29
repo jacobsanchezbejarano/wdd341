7a96a92d843667105fe9b79223c2bce068492a16
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-04-29 03:03:21 -0400

    fix

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const hostname = '127.0.0.1';
 app.use('/', require('./routes'))
   .use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-  res.setHeader('Content-Type','application/json');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  // res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+  // res.setHeader('Content-Type','application/json');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
   })
   .use(bodyParser.json())
