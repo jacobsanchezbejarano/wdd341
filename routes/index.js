285f8e4eff8b812dd3528b8d5378e73d2986bede
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-04-29 20:18:16 -0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ const express = require('express');
 const routes = express.Router();
 const functions = require('../controllers/');
 
+routes.use('/contacts', require('./contacts'));
 routes.use('/', require('./swagger'));
 routes.get('/', functions.get_name);
-routes.use('/contacts', require('./contacts'));
+
 
 
 
